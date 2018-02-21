269c349b4cae29f76eac33ac3ae725901680038e
barni211 <barni211@gmail.com>
2018-02-21 22:05:05 +0100

    Added library services to support database crud operations. First commit after repository created

---
+++ LibraryData/Model/Patron.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Model
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
+
+
+    }
+}
